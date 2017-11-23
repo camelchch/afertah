7fe221127733a1904d9553461449d00289b8c75b
BOUREGAA <bouregaa@MacBook-Pro-de-BOUREGAA.local>
2017-11-23 20:39:40 +0100

    corrig

--- libft/srcs/ft_lstnew.c
+++ libft/srcs/ft_lstnew.c
@@ -1,19 +1,21 @@
 #include "libft.h"
-
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	t_list *list;
+	t_list	*list;
+	char	*copy;
+
 	list = malloc(sizeof(t_list));
-	char const *str;
-	str = (char const *)content;
-	list->content = str;
+	if (!list)
+		return (NULL);
+	copy = malloc(sizeof(char) * (strlen((char*)content)));
+	list->content = copy;
+	ft_memcpy(copy, content, content_size);
 	list->content_size = content_size;
 	list->next = NULL;
 	if (content == NULL)
 	{
 		list->content = NULL;
 		list->content_size = 0;
-		list->next = NULL;
 	}
 	return (list);
 }
